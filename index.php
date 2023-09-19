79a9078978d2ffb418c5e2f5f332c8702471e2a7
Chris Barth <cbarth@gfak.info>
2023-09-20 00:10:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/header.php'; ?>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <?php include 'includes/navigation.php'; ?>
+
+    <!-- Landing page content goes here -->
+
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
